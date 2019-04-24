9d0b99fd5b3551d6365e93c251332dd0f81c7cd4
Mariana Prazeres <mari_p@live.com.pt>
2019-04-23 23:27:47 -0400

    Easy deep learning model

--- deep_learning/model/CNN.py
+++ deep_learning/model/CNN.py
@@ -1,3 +1,30 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
+
+class TwoLayerNet(torch.nn.Module):
+    def __init__(self, D_in, H, D_out):
+        """
+        In the constructor we instantiate two nn.Linear modules and assign them as
+        member variables.
+        """
+        super(TwoLayerNet, self).__init__()
+        self.linear1 = torch.nn.Linear(D_in, H)
+        self.linear2 = torch.nn.Linear(H, 100)
+        self.linear3 = torch.nn.Linear(100, D_out)
+
+    def forward(self, x):
+        """
+        In the forward function we accept a Tensor of input data and we must return
+        a Tensor of output data. We can use Modules defined in the constructor as
+        well as arbitrary operators on Tensors.
+        """
+        print(x.type())
+        h_relu = self.linear1(x).clamp(min=0)
+
+        h2_relu = self.linear2(h_relu).clamp(min=0)
+
+        y_pred = self.linear2(h2_relu)
+        return y_pred
 
--- deep_learning/train.py
+++ deep_learning/train.py
@@ -1,1 +1,53 @@
 import torch
+import pandas as pd
+from model.CNN import TwoLayerNet
+
+
+# N is size of the training set; D_in is input dimension;
+# H is hidden dimension; D_out is output dimension.
+N, D_in, H, D_out = 50, 5, 100, 2
+
+# Data
+PT_data = pd.read_excel("../PTResults_trimmed.xlsx")
+PT_tensor = torch.tensor(PT_data.values)
+
+#Eventually change to batch training
+x_train = PT_tensor[:N,1:-1].float()
+y_train = PT_tensor[:N,-1].long()#.view(N,1)
+
+x_test = PT_tensor[N:,1:-1].float()
+y_test = PT_tensor[N:,-1].long()#.view(N,1)
+
+# Construct our model by instantiating the class defined above
+model = TwoLayerNet(D_in, H, D_out)
+
+# Construct our loss function and an Optimizer. The call to model.parameters()
+# in the SGD constructor will contain the learnable parameters of the two
+# nn.Linear modules which are members of the model.
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
+for t in range(500):
+    # Forward pass: Compute predicted y by passing x to the model
+    y_pred = model(x_train)
+
+    # Compute and print loss
+    loss = criterion(y_pred, y_train)
+    print(t, loss.item())
+
+    # Zero gradients, perform a backward pass, and update the weights.
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    # Test accuracy 
+    correct_train = (torch.argmax(y_pred, dim=-1)==y_train).sum()
+    acc_train = correct_train.float()/y_train.shape[0]
+
+    y_hat = model(x_test)
+    correct_test = (torch.argmax(y_hat, dim=-1)==y_test).sum()
+    acc_test = correct_test.float()/y_test.shape[0]
+
+    print('Training accuracy: ', acc_train.item())
+    print('Testing accuracy: ', acc_test.item())
+
+
